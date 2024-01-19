0a2f7ce247e7a696ef4f1731bf2294c1a2550da0
Anant Majhi <anmajhi7@gmail.com>
2024-01-19 20:10:49 +0530

    loops topic added

---
+++ src/com/04-loops/programs/NestedForExample.java
@@ -0,0 +1,14 @@
+
+//use this example to imagine the indexes of a 2D Array
+public class NestedForExample {
+    public static void main(String[] args) {
+        //loop of i
+        for (int i = 0; i < 3; i++) {
+            //loop of j
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i + "" + j +" ");
+            }//end of i
+            System.out.println();
+        }//end of j
+    }
+}  
